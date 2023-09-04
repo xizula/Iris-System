7fef1f8490cccf12cedb636d45c08b780974300f
xizula <iza.majchrowska12@gmail.com>
2023-09-04 19:01:43 +0200

    change

--- functions.py
+++ functions.py
@@ -3,7 +3,6 @@ import os
 import csv
 from pathlib import Path
 
-threshold = 0.3
 
 def delete_all():
     folder = Path("ubiris/Sessao_1")
@@ -34,9 +33,9 @@ def enroll_all():
 
 
 
-def verify(temp1, temp2):
+def verify(temp1, temp2, threshold):
     v = np.logical_xor(temp1, temp2)
-    ham = np.count_nonzero(v)
+    ham = round(np.count_nonzero(v),2)
     ile = temp1.shape[0]*temp2.shape[1]
     value = ham/ile
     if value <= threshold:
@@ -44,7 +43,7 @@ def verify(temp1, temp2):
     else:
         return value, 0
 
-def createCSV():
+def createCSV(threshold):
     with open('results.csv', 'w', newline='') as file:
         writer = csv.writer(file)
         field = ["Probe 1", "Probe 2", "Passed?", "Should?", "Hamming"]
@@ -66,66 +65,16 @@ def createCSV():
                     probe.close()
                     if ver.id == enrolled.id and ver.probeID == enrolled.probeID:
                         continue
-                    value, res = verify(enrolled.template, ver.template)
+                    value, res = verify(enrolled.template, ver.template, threshold)
                     if ver.id == enrolled.id:
                         writer.writerow(["`" + enrolled.id + "/" + enrolled.probeID, "'" +ver.id + "/" + ver.probeID, res, 1, value])
                     else:
                         writer.writerow(["`" +enrolled.id + "/" + enrolled.probeID,"'" + ver.id + "/" + ver.probeID, res, 0, value])
 
-    # with open('results.csv', 'w', newline='') as file:
-    #     writer = csv.writer(file)
-    #     field = ["Probe 1", "Probe 2", "Passed?", "Should?", "Hamming"]
-    #     writer.writerow(field)
-    #     for i in range(1,242):
-    #         folder = "ubiris/Sessao_1/" + str(i)
-    #         probe_name = []
-    #         # **** Find enrolled probe **** #
-    #         for image in os.listdir(folder):
-    #             if (image.endswith(".pkl")):
-    #                 probe_name.append(image)
-    #         if len(probe_name) == 0:
-    #             continue
-    #         else:
-    #             path = folder + "/" + str(probe_name[0]) # enrolled one
-    #             file = open(path, 'rb')
-    #             enrolled = pickle.load(file)
-    #             file.close()
-    #         # **** Verify enrolled with others **** #
-    #         for j in range(1,242):
-    #             folder = "ubiris/Sessao_1/" + str(j)
-    #             probe_name = []
-    #             for image in os.listdir(folder):
-    #                 if (image.endswith(".pkl")):
-    #                     probe_name.append(image)
-    #             for k in range(len(probe_name)):
-    #                 path = folder + "/" + str(probe_name[k])
-    #                 file = open(path, 'rb')
-    #                 ver = pickle.load(file)
-    #                 file.close()
-    #                 if ver.id == enrolled.id and ver.probeID == enrolled.probeID:
-    #                     continue
-    #                 value, res = verify(enrolled.template, ver.template)
-    #                 if ver.id == enrolled.id:
-    #                     writer.writerow([res, 1, value])
-    #                 else:
-    #                     writer.writerow([res, 0, value])
 
-        
+# def count_FAR(file_path):
+           
+delete_all()
+enroll_all()
+createCSV(0.3)
 
-
-# file = open('ubiris/Sessao_1/5/1.pkl', 'rb')
-# x = pickle.load(file)
-# file.close()
-# file = open('ubiris/Sessao_1/5/4.pkl', 'rb')
-# y = pickle.load(file)
-# file.close()
-
-# value, bo = verify(x.template, y.template)
-# print( value, bo)
-
-# print(value, bo)
-# createCSV()
-# print(verify(x.template, y.template))
-# delete_all()
-# enroll_all()
-createCSV()
